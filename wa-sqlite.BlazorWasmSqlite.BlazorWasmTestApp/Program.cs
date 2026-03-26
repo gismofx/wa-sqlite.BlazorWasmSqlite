84de565ddfe95be3827cae71122d06b168f9afb3
Chad Posner <chadposner@gmail.com>
2026-03-25 20:17:04 -0400

    Cleanup test app

--- wa-sqlite.BlazorWasmSqlite.BlazorWasmTestApp/Program.cs
+++ wa-sqlite.BlazorWasmSqlite.BlazorWasmTestApp/Program.cs
@@ -15,9 +15,12 @@ namespace wa_sqlite.BlazorWasmSqlite.BlazorWasmTestApp
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            
+
+
             builder.Services.AddSingleton(x => new SqliteWasmConnectionStringBuilder() { DatabaseName = "MyApp", Filename = "MyFile" });
-            builder.Services.AddSingleton<SqliteWasmInterop>();
-            builder.Services.AddSingleton<SqliteWasmConnection>();
+            builder.Services.AddSqliteWasmInterop();
+
 
             builder.Services.AddMudServices();
 
